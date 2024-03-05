f4b2a5c198c15b97c8dd61cda64ddbbb9d0f6c4c
Timon Reinmann <timon.reinmann@yoo.digital>
2024-03-05 08:44:53 +0100

    feat: ThePrimeagen Setup

---
+++ init.lua
@@ -0,0 +1,5 @@
+-- Füge ein spezifisches Verzeichnis auf einem Windows-System hinzu
+package.path = package.path .. ";C:\\Users\\TimonReinmann\\AppData\\Local\\nvim\\lua\\theprimeagen"
+
+require("theprimeagen")
+
